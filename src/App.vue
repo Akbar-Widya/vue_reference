d686e8d932cfedec8ee7d22255c32924fde0fc49
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-09 20:08:47 +0700

    topic: conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { ref } from 'vue'
 
-const text = ref('')
-function onInput(e) {
-   text.value = e.target.value
+const awesome = ref(true)
+
+function toggle() {
+  awesome.value = !awesome.value
 }
 </script>
 
 <template>
-   <!-- <input :value="text" @input="onInput" placeholder="Type here" /> -->
-   or just this : <input v-model="text" placeholder="Type here" />
-   <p>{{ text }}</p>
+  <button @click="toggle">Toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
 </template>
