6d52589ccf6cedc576bf726ac689d56f687be904
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-09 19:52:41 +0700

    topic: attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref } from 'vue'
 
-// component logic
-// declare some reactive state here.
-const counter = reactive({
-   count: 0,
-});
-console.log(counter.count);
-counter.count++;
-
-const message = ref("Greet Me?");
-console.log(message.value);
-message.value = "Hello world!";
+const titleClass = ref('title')
 </script>
 
 <template>
-   <h1>{{ message }}</h1>
-   <p>Count is: {{ counter.count }}</p>
+  <h1 :class="titleClass">Make me red</h1>
 </template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
