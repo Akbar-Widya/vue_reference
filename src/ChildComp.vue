5b9c098f57d6e03ecf0e8849a746afe7d64c8308
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-10 17:24:29 +0700

    topic: slots. apart from props, parent can also pass down data through slots. The data will be renderon the slot tag inside child comp. But the fallback content will be declared in the child component instead.

--- src/ChildComp.vue
+++ src/ChildComp.vue
@@ -1,9 +1,6 @@
-<script setup>
-const emit = defineEmits(['response'])
-
-emit('response', 'hello from child')
-</script>
-
 <template>
-   <h2>Child component</h2>
+  <slot>Fallback content</slot>
+  <p>
+    hello, this is child comp data
+  </p>
 </template>
